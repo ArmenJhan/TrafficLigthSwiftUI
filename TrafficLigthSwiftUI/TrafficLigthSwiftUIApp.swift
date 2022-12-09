2f5d34527370652f5984d11f9af7f67339c20b1d
Armen <madarmen1987@gmail.com>
2022-12-09 16:56:57 +0400

    Initial Commit

---
+++ TrafficLigthSwiftUI/TrafficLigthSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLigthSwiftUIApp.swift
+//  TrafficLigthSwiftUI
+//
+//  Created by Armen Madoyan on 09.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLigthSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
